1218f59062819d8e2811c35544541d56c7b6fb8c
pbhuyar <131512700+pbhuyar@users.noreply.github.com>
2025-02-17 16:16:52 +0530

    Add files via upload
    JAVA OOPS Programs

---
+++ MainClass.java
@@ -0,0 +1,17 @@
+class MainClass 
+	{
+	 	public static void main (String args [] )
+		{
+		  SciFiCalculator sci = new SciFiCalculator(); 
+	          System.out.println(sci.add(10,20));
+		  System.out.println(sci.sub(10,20));
+		  System.out.println(sci.mul(10,20));
+		  System.out.println(sci.div(10,20));
+		  System.out.println(sci.square(5));
+		  System.out.println(sci.cube(5));
+		  System.out.println(sci.squareRoot(25));
+		  System.out.println(sci.cubeRoot(900));
+		}
+	}
+	
+	
---
+++ MainClass2.java
@@ -0,0 +1,9 @@
+
+class MainClass2 
+{
+	public static void main (String args [] )
+	{
+		Demo1 d1 = new Demo2();
+		 d1.m1();
+	}
+}
